fd96854cff5db9ab82373b8274faeab9f8de86f4
ryabad <ryabaDa@gmail.com>
2023-11-11 05:25:44 +0200

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleClick = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = (good, neutral, bad) => {
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    const total = this.countTotalFeedback(good, neutral, bad);
+    return Math.round((good / total) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Section title="Please leave feedback" className="section">
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleClick}
+        />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={this.countTotalFeedback}
+          positivePercentage={this.countPositiveFeedbackPercentage}
+        />
+      </Section>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/index.js
@@ -0,0 +1,17 @@
+import css from './index.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(el => (
+    <button
+      className={css.btn}
+      key={el}
+      type="button"
+      value={el}
+      onClick={() => onLeaveFeedback(el)}
+    >
+      {el}
+    </button>
+  ));
+};
+
+export default FeedbackOptions;
